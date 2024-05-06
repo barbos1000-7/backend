f5b6ef455dd12c6e4a860a46db0f3c0550cf6919
amin <awermin@mail.ru>
2024-05-06 20:50:44 +0500

    why????

--- database/index.js
+++ database/index.js
@@ -4,7 +4,7 @@ const DBSOURCE = "db.sqlite"
 
 const db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
-        console.log('Connected to the SQLite database.')
+        console.log('Coerrrororororororororoororororororororororororororororororororoorororororororororororoororororor.')
         // Cannot open database
         console.error(err.message)
         throw err
