78adc8cc0eeedfdfe3b397eaf02ef83eef67cf64
amin <awermin@mail.ru>
2024-05-06 21:03:11 +0500

    why????

--- database/index.js
+++ database/index.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose()
+const path = require('path');
 
-const DBSOURCE = "../db.sqlite"
-
+const DBSOURCE = path.join(__dirname, 'db.sqlite')
 const db = new sqlite3.Database(DBSOURCE, (err) => {
     if (err) {
         console.log('Coerrrororororororororoororororororororororororororororororororoorororororororororororoororororor.')
