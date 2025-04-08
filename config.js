2db02b909d0e39ecfe65431c0297328146a80080
barbos1000-7 <116086904+barbos1000-7@users.noreply.github.com>
2025-04-08 21:10:23 +0500

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    secret: "ertwetrwetweterqwrqeQ  W   ",
-    SMTP_HOST: 'smtp.yandex.ru',
-    SMPT_PORT: '465',
-    SMTP_USER: "youngteaschool@yandex.ru",
-    SMTP_PASS: 'xtfdrttryaoefbig',
-    staticPath: 'C:\\Users\\amin\\react\\projects\\backend\\backend\\static'
-}
